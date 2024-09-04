8d6fbf9bdbd588268a5d6050e02add6d1ccc4ce0
Michael Wayne <iymw>
2024-09-04 10:38:10 +0700

    feat: rmeove title and desc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Typography from "@/Typography";
 import { Link, Button } from "@nextui-org/react";
 import React from "react";
 
@@ -6,21 +5,7 @@ const page = () => {
   return (
     <>
       <div className="flex flex-col gap-8 h-screen w-full text-center justify-center items-center">
-        <Typography
-          variant="h1"
-          weight="bold"
-          className="bg-gradient-to-r from-[#FC466B] to-[#3F5EFB] inline-block text-transparent bg-clip-text"
-        >
-          K-K7: Your Ultimate <br /> Encryption Ally
-        </Typography>
-        <Typography className="w-[60%] mx-auto">
-          Unlock the power of secure communication with K-K7. Seamlessly encrypt
-          or decrypt your data with confidence, knowing your information is
-          shielded by the latest in cryptographic technology. Whether it's
-          protecting your private messages or securing sensitive information,
-          K-K7 is your go-to tool for airtight security.
-        </Typography>
-        <div className="space-x-4">
+        <div className="flex sm:flex-row flex-col gap-4">
           <Button color="primary" variant="bordered" href="/ebc" as={Link}>
             Electronic Code Book
           </Button>
