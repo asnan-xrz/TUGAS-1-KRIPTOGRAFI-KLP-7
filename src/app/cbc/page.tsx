5bb145f188497dd147c345fc33e1981a05f129a5
Michael Wayne <iymw>
2024-09-04 21:39:53 +0700

    feat: cbc

--- src/app/cbc/page.tsx
+++ src/app/cbc/page.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import { Button, Input, Textarea } from "@nextui-org/react";
-import React from "react";
+import React, { useState } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 
 type Inputs = {
   plaintext: string;
   key: string;
   vector: string;
+  result: string;
 };
 
 const Page = () => {
@@ -16,12 +17,65 @@ const Page = () => {
     handleSubmit,
     formState: { errors },
   } = useForm<Inputs>();
-  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
+  const [decryptResult, setDecryptResult] = useState<string>("");
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    const { plaintext, vector, key }: any = data;
+    let vectorToBinary: any = "";
+    let keyToBinary: any = "";
+
+    let toBinary: any = [];
+
+    for (let i = 0; i < plaintext.length; i++) {
+      toBinary[i] = parseInt(plaintext[i], 16).toString(2).padStart(4, "0");
+    }
+
+    for (let i = 0; i < vector.length; i++) {
+      vectorToBinary +=
+        vectorToBinary + parseInt(vector[i], 16).toString(2).padStart(4, "0");
+    }
+
+    for (let i = 0; i < key.length; i++) {
+      keyToBinary += parseInt(key[i], 16).toString(2).padStart(4, "0");
+    }
+
+    let cToString: any = [];
+
+    let p: any = [];
+    let c: any = [];
+
+    for (let i = 0; i < plaintext.length; i++) {
+      if (i === 0) {
+        p[i] = toBinary[i] ^ vectorToBinary;
+        p[i] ^= keyToBinary;
+        p[i] = p[i] << 1;
+        c[i] = p[i];
+        cToString[i] = c[i];
+      } else {
+        toBinary[i] = parseInt(toBinary[i], 2);
+        p[i] = toBinary[i] ^ c[i - 1];
+        p[i] = p[i].toString(2).padStart(4, "0");
+        p[i] = (parseInt(p[i], 2) ^ parseInt(keyToBinary, 2)).toString(2);
+        p[i] = p[i].slice(1) + p[i][0];
+        p[i] = parseInt(p[i], 2);
+        c[i] = p[i];
+        cToString[i] = c[i].toString(2).padStart(4, "0");
+      }
+    }
+
+    let result: string = "";
+    for (let i = 0; i < plaintext.length; i++) {
+      c[i] = c[i].toString(16).toUpperCase();
+      result += c[i];
+    }
+
+    setDecryptResult(result);
+  };
 
   return (
     <>
       <section className="flex flex-col gap-8 h-screen justify-center items-center">
-        <p className="absolute z-[-1] text-8xl"></p>
         <form onSubmit={handleSubmit(onSubmit)}>
           <div className="space-y-4 w-[400px]">
             <Textarea
@@ -32,23 +86,23 @@ const Page = () => {
               className="col-span-12 md:col-span-6 mb-6 md:mb-0"
               errorMessage={errors.plaintext && "Plain text is required"}
               isInvalid={errors.plaintext ? true : false}
-              {...register("plaintext", { required: true })}
-            />
-            <Input
-              type="text"
-              variant="bordered"
-              label="Key"
-              {...register("key", { required: true })}
-              errorMessage={errors.key && "Key field is required"}
-              isInvalid={errors.key ? true : false}
+              {...register("plaintext", { required: false })}
             />
             <Input
               type="text"
               variant="bordered"
               label="Vector"
-              {...register("vector", { required: true })}
+              {...register("vector", { required: false })}
               errorMessage={errors.vector && "Vector field is required"}
               isInvalid={errors.vector ? true : false}
+            />
+            <Input
+              type="text"
+              variant="bordered"
+              label="Key"
+              {...register("key", { required: false })}
+              errorMessage={errors.key && "Key field is required"}
+              isInvalid={errors.key ? true : false}
             />
             <Textarea
               variant="flat"
@@ -56,6 +110,7 @@ const Page = () => {
               labelPlacement="inside"
               readOnly
               className="col-span-12 md:col-span-6 mb-6 md:mb-0"
+              value={decryptResult}
             />
           </div>
           <br />
